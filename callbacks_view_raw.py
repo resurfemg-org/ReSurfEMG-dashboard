2c2eed9ed544883eca84576218782f1622004ab5
rspwarnaarUT <r.s.p.warnaar@utwente.nl>
2025-04-13 21:11:19 +0200

    Add units and labels to raw. WIP linked X-zoom

--- callbacks_view_raw.py
+++ callbacks_view_raw.py
@@ -5,10 +5,11 @@ Licensed under the Apache License, version 2.0. See LICENSE for details.
 This file contains functions to work functions from the ReSurfEMG library.
 """
 
-from dash import Input, Output, callback, ctx, MATCH, State
+from dash import Input, Output, callback, ctx, MATCH, State, ALL, callback_context
 from app import app, variables
 import utils
 import numpy as np
+from dash.exceptions import PreventUpdate
 
 
 @callback(Output('emg-graphs-container', 'children'),
@@ -17,6 +18,13 @@ import numpy as np
           Input('emg-delete-button', 'n_clicks'))
 def show_raw_data(delete):
     emg_data = variables.get_emg()
+    emg_timeseries = variables.get_emg_timeseries()
+    if emg_timeseries is not None:
+        titles = [ts.label for ts in emg_timeseries]
+        units = [ts.y_units for ts in emg_timeseries]
+    else:
+        titles = None
+        units = None
     hidden = True
 
     trigger_id = ctx.triggered_id
@@ -30,7 +38,8 @@ def show_raw_data(delete):
     else:
         if emg_data is not None:
             emg_frequency = variables.get_emg_freq()
-            children_emg = utils.add_emg_graphs(np.array(emg_data), emg_frequency)
+            children_emg = utils.add_emg_graphs(
+                np.array(emg_data), emg_frequency, titles=titles, units=units)
             hidden = False
         else:
             children_emg = []
@@ -66,10 +75,43 @@ def show_raw_data(delete):
 
 
 @app.callback(
-    Output({"type": "dynamic-updater", "index": MATCH}, "updateData"),
-    Input({"type": "dynamic-graph", "index": MATCH}, "relayoutData"),
-    State({"type": "dynamic-graph", "index": MATCH}, "id"),
+    Output({"type": "dynamic-updater", "index": ALL}, "updateData"),
+    Input({"type": "dynamic-graph", "index": ALL}, "relayoutData"),
+    State({"type": "dynamic-graph", "index": ALL}, "id"),
     prevent_initial_call=True,
 )
-def update_figure(relayoutdata: dict, graph_id_dict: dict):
-    return utils.get_dict(graph_id_dict, relayoutdata)
+def update_figure(relayoutdata_list: dict, graph_id_dict_list: dict):
+    triggered_index = callback_context.triggered[0]["prop_id"].split(
+        '{"index":"')[1].split('","type')[0]
+    graph_id_list = [d["index"] for d in graph_id_dict_list]
+    src_idx = graph_id_list.index(triggered_index)
+    relayoutdata_src = relayoutdata_list[src_idx]
+    if relayoutdata_src is not None and 'xaxis.range[0]' in relayoutdata_src:
+        x_range_new = [relayoutdata_src['xaxis.range[0]'],
+                       relayoutdata_src['xaxis.range[1]']]
+
+    updateData_list = []
+    for idx, key in enumerate(graph_id_list):
+        relayoutdata = relayoutdata_list[idx]
+        graph_id_dict = graph_id_dict_list[idx]
+        if relayoutdata is not None:
+            if (relayoutdata_src is not None
+                    and 'xaxis.range[0]' in relayoutdata_src):
+                relayoutdata.pop('xaxis.autorange', None)
+                relayoutdata.pop('xaxis.showspikes', None)
+                relayoutdata['xaxis.range[0]'] = x_range_new[0]
+                relayoutdata['xaxis.range[1]'] = x_range_new[1]
+                fig = utils.get_graph_fig(dict_key=key)
+                fig.update_xaxes(range=x_range_new, overwrite=True)
+                fig.update_xaxes(showgrid=False)
+            else:
+                relayoutdata['xaxis.autorange'] = True
+                relayoutdata['xaxis.showspikes'] = True
+                relayoutdata.pop('xaxis.range[0]', None)
+                relayoutdata.pop('xaxis.range[1]', None)
+                fig = utils.get_graph_fig(dict_key=key)
+                fig.update_xaxes(autorange=True, overwrite=True)
+        updateData = utils.get_dict(graph_id_dict, relayoutdata)
+        updateData_list.append(updateData)
+
+    return updateData_list
